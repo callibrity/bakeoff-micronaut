d0184395163ec1a0eda8a34a0fb5956888bf36ae
James Carman <jcarman@callibrity.com>
2022-09-09 08:52:26 -0400

    Refactoring to introduce a service.

---
+++ src/main/java/com/callibrity/bakeoff/ArtistService.java
@@ -0,0 +1,55 @@
+package com.callibrity.bakeoff;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+
+import javax.transaction.Transactional;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+@Singleton
+public class ArtistService {
+
+// ------------------------------ FIELDS ------------------------------
+
+    @Inject
+    private ArtistRepository repository;
+
+// -------------------------- OTHER METHODS --------------------------
+
+    @Transactional
+    public Artist createArtist(String name, Genre genre) {
+        final Artist artist = new Artist();
+        artist.setName(name);
+        artist.setGenre(genre);
+        repository.save(artist);
+        return artist;
+    }
+
+    @Transactional
+    public void deleteArtistById(String id) {
+        repository.deleteById(id);
+    }
+
+    public Optional<Artist> findArtistById(String id) {
+        return repository.findById(id);
+    }
+
+    public List<Artist> listAllArtists() {
+        final List<Artist> artists = new LinkedList<>();
+        repository.findAll().forEach(artists::add);
+        return artists;
+    }
+
+    @Transactional
+    public Optional<Artist> updateArtist(String id, String name, Genre genre) {
+        return repository.findById(id)
+                .map(artist -> {
+                    artist.setName(name);
+                    artist.setGenre(genre);
+                    return artist;
+                });
+    }
+
+}
--- src/main/java/com/callibrity/bakeoff/ArtistsController.java
+++ src/main/java/com/callibrity/bakeoff/ArtistsController.java
@@ -5,48 +5,43 @@ import io.micronaut.http.annotation.*;
 import io.micronaut.http.exceptions.HttpStatusException;
 import jakarta.inject.Inject;
 
-import java.util.LinkedList;
 import java.util.List;
 
 @Controller("/api/artists")
 public class ArtistsController {
+
+// ------------------------------ FIELDS ------------------------------
+
     @Inject
-    private ArtistRepository repository;
+    private ArtistService service;
 
-    @Get
-    public List<Artist> list() {
-        final List<Artist> artists = new LinkedList<>();
-        repository.findAll().forEach(artists::add);
-        return artists;
-    }
-
-    @Get("/{id}")
-    public Artist get(@PathVariable("id") String id) {
-        return repository.findById(id)
-                .orElseThrow(() -> new HttpStatusException(HttpStatus.NOT_FOUND, "Artist not found."));
-    }
+// -------------------------- OTHER METHODS --------------------------
 
     @Post(consumes = "application/json")
     public Artist create(@Body CreateArtistRequest request) {
-        final Artist artist = new Artist();
-        artist.setName(request.getName());
-        artist.setGenre(request.getGenre());
-        repository.save(artist);
-        return artist;
+        return service.createArtist(request.getName(), request.getGenre());
     }
 
     @Delete("/{id}")
     public void delete(@PathVariable("id") String id) {
-        repository.deleteById(id);
+        service.deleteArtistById(id);
+    }
+
+    @Get("/{id}")
+    public Artist get(@PathVariable("id") String id) {
+        return service.findArtistById(id)
+                .orElseThrow(() -> new HttpStatusException(HttpStatus.NOT_FOUND, "Artist not found."));
+    }
+
+    @Get
+    public List<Artist> list() {
+        return service.listAllArtists();
     }
 
     @Put(value = "/{id}", consumes = "application/json")
     public Artist update(@PathVariable("id") String id, @Body UpdateArtistRequest request) {
-        Artist artist = repository.findById(id)
+        return service.updateArtist(id, request.getName(), request.getGenre())
                 .orElseThrow(() -> new HttpStatusException(HttpStatus.NOT_FOUND, "Artist not found."));
-        artist.setName(request.getName());
-        artist.setGenre(request.getGenre());
-        repository.update(artist);
-        return artist;
     }
+
 }
